ae9739792284e541ece389a46c19c8de38468734
chohanjoo <chohanjoo@github.com>
2019-01-22 13:50:53 +0900

    가입환영 이메일 보내기

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 
 # Create your models here.
@@ -13,5 +14,13 @@ def on_post_save_for_user(sender, **kwargs):
     if kwargs['created']:
         user = kwargs['instance']
         Profile.objects.create(user=user)
-        
+
+        send_mail(
+            'Subject here',
+            'Here is the message.',
+            'vjswl132@gmail.com',
+            ['johanjoo@naver.com'],
+            fail_silently=False,
+        )
+
 post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
--- sodasite/settings.py
+++ sodasite/settings.py
@@ -129,3 +129,14 @@ LOGIN_URL = reverse_lazy('login')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 LOGIN_REDIRECT_URL = reverse_lazy('profile')
+
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'user_name@gmail.com'
+EMAIL_HOST_PASSWORD = 'user_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
