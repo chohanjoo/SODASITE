6550b6c197202f57e2b94548502e4da49ce9b413
chohanjoo <chohanjoo@github.com>
2019-02-11 18:35:44 +0900

    delete all files

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['id','title']
-    list_display_links = ['title']
