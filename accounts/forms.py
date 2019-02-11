4cbcc05582b7869c368d6591811f2e2c92cde903
chohanjoo <chohanjoo@github.com>
2019-02-11 17:37:17 +0900

    Changes to be committed: 	modified:   ../about/__pycache__/__init__.cpython-37.pyc 	modified:   ../about/__pycache__/admin.cpython-37.pyc 	modified:   ../about/__pycache__/models.cpython-37.pyc 	modified:   ../about/__pycache__/urls.cpython-37.pyc 	modified:   ../about/__pycache__/views.cpython-37.pyc 	modified:   ../about/migrations/__pycache__/__init__.cpython-37.pyc 	modified:   ../accounts/__pycache__/admin.cpython-37.pyc 	modified:   ../accounts/__pycache__/forms.cpython-37.pyc 	modified:   ../accounts/__pycache__/models.cpython-37.pyc 	modified:   ../accounts/__pycache__/urls.cpython-37.pyc 	modified:   ../accounts/__pycache__/views.cpython-37.pyc 	modified:   ../accounts/forms.py 	modified:   ../accounts/migrations/__pycache__/0001_initial.cpython-37.pyc 	modified:   ../blog/__pycache__/admin.cpython-37.pyc 	modified:   ../blog/__pycache__/models.cpython-37.pyc 	modified:   ../blog/__pycache__/urls.cpython-37.pyc 	modified:   ../blog/__pycache__/views.cpython-37.pyc 	modified:   ../blog/migrations/__pycache__/0001_initial.cpython-37.pyc 	modified:   ../db.sqlite3

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
 class SignupForm(UserCreationForm):
-    pass
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
