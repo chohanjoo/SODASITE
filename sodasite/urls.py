7ae3a38780a6094265fc5aec777b9ca41ca264f8
chohanjoo <johanjoo@naver.com>
2019-02-26 00:57:03 +0900

    사진 및 파일 업로드 가능 및  라인정리

--- sodasite/urls.py
+++ sodasite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +33,5 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
 
     ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
